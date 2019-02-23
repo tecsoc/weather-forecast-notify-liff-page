541cb799a9b2807ee5e2fc3c998d25b93f7c0dcf
TKTK-ST <kaiketsu.genki@gmail.com>
2019-02-23 10:48:53 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
+var mainView;
+
 window.onload = function() {
+    mainView = document.getElementById("main");
     if (navigator.userAgent.indexOf("Line") !== -1) {
         alert("さては、、お前、LINEから開いたな？？");
     }else{
-        alert("PCは非対応");
+        alert("PCは非対応です");
+        mainView.style.display="none";
     }
 }
