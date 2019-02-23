8ffeb3b0bfd5cc2232d5a26643d51d027cb73a35
TKTK-ST <kaiketsu.genki@gmail.com>
2019-02-23 20:30:03 +0900

    Update main.js

--- main.js
+++ main.js
@@ -5,9 +5,30 @@ window.onload = function() {
     mainView = document.getElementById("main");
     resultView = document.getElementById("result");
     if (navigator.userAgent.indexOf("Line") !== -1 || true) {
-        alert("さては、、お前、LINEから開いたな？？");
+        liff.init(function (data) {
+            var userId = data.context.userId;
+            alert("ユーザーID：" + serId);
+        }, function(error) {
+        alert(error);
+        });
     }else{
         mainView.style.display="none";
         resultView.innerHTML = "スマホアプリのLINEから開いてください！！<br>PCその他から設定することは出来ません。";
     }
+}
+
+function selectALl(){
+    
+}
+
+function deselectAll(){
+    
+}
+
+function selectWorkday(){
+    
+}
+
+function sumbit(){
+    liff.closeWindow();
 }
