92f77c5ca8693a19cbdb5abdbe4a3361eaf0054f
TKTK-ST <kaiketsu.genki@gmail.com>
2019-03-25 16:45:25 +0900

    Update main.js

--- main.js
+++ main.js
@@ -59,7 +59,7 @@ function sumbit(){
         alert(error);
         return;
     }
-    //liff.closeWindow();
+    setTimeout(function(){ liff.closeWindow(); , 1000);
     /*.then(function() {
         window.alert("送信完了");
         // aleartでOKを押すと、自動でLIFFウィンドウが閉じる
