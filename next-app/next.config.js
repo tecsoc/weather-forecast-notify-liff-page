cb007fb70f88bf38fcf7f52c46366bd8830969ec
t-tecsoc <25559838+t-tecsoc@users.noreply.github.com>
2023-09-02 10:26:18 +0900

    setup github actions

--- next-app/next.config.js
+++ next-app/next.config.js
@@ -1,11 +1,17 @@
 const path = require('path');
 
 /** @type {import('next').NextConfig} */
- 
+const isProduction = process.env.NODE_ENV === "production";
+const repository = process.env.GITHUB_REPOSITORY;
+const repositoryOwner = process.env.GITHUB_REPOSITORY_OWNER;
+const repositoryName = repository?.replace(repositoryOwner, "");
+
 const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  assetPrefix: isProduction ? repositoryName : "",
+  output: "export"
 };
 
 module.exports = nextConfig
