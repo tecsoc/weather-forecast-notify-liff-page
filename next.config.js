86033a68beb0c66e32fb69951687bfc872f2b7cf
t-tecsoc <25559838+t-tecsoc@users.noreply.github.com>
2023-10-15 11:06:07 +0900

    staging deployに対応

--- next.config.js
+++ next.config.js
@@ -1,16 +1,27 @@
 const path = require("path");
 
-/** @type {import('next').NextConfig} */
 const isProduction = process.env.NODE_ENV === "production";
 const repository = process.env.GITHUB_REPOSITORY;
 const repositoryOwner = process.env.GITHUB_REPOSITORY_OWNER;
 const repositoryName = repository?.replace(repositoryOwner, "");
+const branch = process.env.GITHUB_HEAD_REF;
+const getAssetPrefix = () => {
+  if (isProduction) {
+    if (repositoryName === "main") {
+      return repositoryName;
+    } else {
+      return `${repositoryName}/${branch}`;
+    }
+  }
+  return "";
+};
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
-  assetPrefix: isProduction ? repositoryName : "",
+  assetPrefix: getAssetPrefix(),
   output: "export",
 };
 
