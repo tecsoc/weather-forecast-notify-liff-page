98a8227b194c2b56e40097a352c984587257893b
t-tecsoc <25559838+t-tecsoc@users.noreply.github.com>
2023-11-06 06:55:27 +0900

    Revert "Merge pull request #3 from tecsoc/staging"
    This reverts commit 8ef272d90b17c2a3d405ff742af07eab4a8492ae, reversing
    changes made to b3207ce290ee879046a32ffb93a134383ab16a06.

--- next.config.js
+++ next.config.js
@@ -1,29 +1,16 @@
 const path = require("path");
-const { execSync } = require('child_process');
 
+/** @type {import('next').NextConfig} */
 const isProduction = process.env.NODE_ENV === "production";
 const repository = process.env.GITHUB_REPOSITORY;
 const repositoryOwner = process.env.GITHUB_REPOSITORY_OWNER;
 const repositoryName = repository?.replace(repositoryOwner, "");
-const baseUrl =  `/${repositoryName}`;
-const currentBranch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
-const getAssetPrefix = () => {
-  if (isProduction) {
-    if (repositoryName === "main") {
-      return baseUrl;
-    } else {
-      return `${baseUrl}/${currentBranch}`;
-    }
-  }
-  return "";
-};
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
-  assetPrefix: getAssetPrefix(),
+  assetPrefix: isProduction ? repositoryName : "",
   output: "export",
 };
 
